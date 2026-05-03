7dab18ba26436033c4c2cced9f7734a3eef421a4
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-05-03 22:56:48 +0300

    Update AllTests.cpp

--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -1,9 +1,13 @@
-// test/AllTests.cpp (пример)
+// Copyright 2026 <Student Name>
+
+#include <iostream>
+
 void testPriorityQueue();
 void testEmptyQueue();
 
 int main() {
     testPriorityQueue();
     testEmptyQueue();
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
